2fd23d4698448ff4b00fa562b208926d06001758
Serena <serenafayelombardo@gmail.com>
2021-06-05 14:39:40 +0200

    Added faded background when opening menu and refactored JS

--- js/main.js
+++ js/main.js
@@ -1,35 +1,43 @@
 //Menu Functionality
 const menuButton = document.querySelector('.header__nav__menu-button');
-const menuButtonImage = menuButton.querySelector('img')
+const menuButtonImage = menuButton.querySelector('img');
 const menu = document.querySelector('.header__nav__menu');
-let menuLinks = document.querySelectorAll('.header__nav__menu__link')
+let menuLinks = document.querySelectorAll('.header__nav__menu__link');
+const backgroundFade = document.querySelector('.background-fade');
+
+function hideMenu() {
+  menu.classList.add('hidden');
+  menuButtonImage.src = 'images/icon-hamburger.svg';
+  menuButton.setAttribute('aria-expanded', 'false');
+  backgroundFade.style.display = 'none';
+}
+
+function showMenu() {
+  menu.classList.remove('hidden');
+  menuButtonImage.src = 'images/icon-close-menu.svg';
+  menuButton.setAttribute('aria-expanded', 'true');
+  backgroundFade.style.display = 'block';
+}
 
 menuButton.addEventListener('click', () => {
-    if (menu.classList.contains('hidden')) {
-        menuButtonImage.src = 'images/icon-close-menu.svg'
-        menuButton.setAttribute('aria-expanded', 'true')
-    } else {
-        menuButtonImage.src = 'images/icon-hamburger.svg'
-        menuButton.setAttribute('aria-expanded', 'false')
-    }
-    menu.classList.toggle('hidden')
-})
+  if (menu.classList.contains('hidden')) {
+    showMenu();
+  } else {
+    hideMenu();
+  }
+});
 
 window.addEventListener('keyup', (event) => {
-    if (event.key === 'Escape' && !menu.classList.contains('hidden')) {
-        menu.classList.add('hidden')
-        menuButtonImage.src = 'images/icon-hamburger.svg'
-        menuButton.setAttribute('aria-expanded', 'false')
-    }
-})
+  if (event.key === 'Escape' && !menu.classList.contains('hidden')) {
+    hideMenu();
+  }
+});
 
-menuLinks.forEach(menuLink => {
-    menuLink.addEventListener('focusout', (event) => {
-        if (event.relatedTarget.classList.contains('header__nav__menu__link')) {
-            return
-        }
-        menu.classList.add('hidden')
-        menuButtonImage.src = 'images/icon-hamburger.svg'
-        menuButton.setAttribute('aria-expanded', 'false')
-    })
-})
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('focusout', (event) => {
+    if (event.relatedTarget.classList.contains('header__nav__menu__link')) {
+      return;
+    }
+    hideMenu();
+  });
+});
